dc91b3dbfa96e12b673ff74d76fe20370780d786
rajamanivinayagam <rajamanivinayagam@gmail.com>
2015-03-17 15:57:25 +0530

    17Mar2015
    so far

---
+++ app/src/main/java/com/dreamerindia/buslocation/MainActivity.java
@@ -0,0 +1,221 @@
+package com.dreamerindia.buslocation;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    private TextView latitude;
+    private TextView longitude;
+    private TextView choice;
+    private CheckBox fineAcc;
+    private Button choose;
+    private TextView provText;
+    private LocationManager locationManager;
+    private String provider;
+    private MyLocationListener mylistener;
+    private Criteria criteria;
+    long lastPress;
+    TextView ll;
+
+    /**
+     * Called when the activity is first created.
+     */
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        latitude = (TextView) findViewById(R.id.lat);
+        longitude = (TextView) findViewById(R.id.lon);
+        provText = (TextView) findViewById(R.id.prov);
+        choice = (TextView) findViewById(R.id.choice);
+        fineAcc = (CheckBox) findViewById(R.id.fineAccuracy);
+        choose = (Button) findViewById(R.id.chooseRadio);
+        ll = (TextView) findViewById(R.id.ll);
+
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the location provider
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);    //default
+
+        // user defines the criteria
+        choose.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (fineAcc.isChecked()) {
+                    criteria.setAccuracy(Criteria.ACCURACY_FINE);
+                    choice.setText("fine accuracy selected");
+                } else {
+                    criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+                    choice.setText("coarse accuracy selected");
+                }
+            }
+        });
+        criteria.setCostAllowed(false);
+        // get the best provider depending on the criteria
+        provider = locationManager.getBestProvider(criteria, false);
+
+        // the last known location of this provider
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        mylistener = new MyLocationListener();
+
+        if (location != null) {
+            mylistener.onLocationChanged(location);
+        } else {
+            // leads to the settings because there is no last known location
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        // location updates: at least 1 meter and 200millsecs change
+        locationManager.requestLocationUpdates(provider, 200, 1, mylistener);
+        new PostDataAsyncTask().execute();
+    }
+
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            // Initialize the location fields
+            latitude.setText("Latitude: " + String.valueOf(location.getLatitude()));
+            longitude.setText("Longitude: " + String.valueOf(location.getLongitude()));
+            provText.setText(provider + " provider has been selected.");
+            ll.setText(location.getLatitude() + "," + location.getLongitude());
+            Toast.makeText(MainActivity.this, "Location changed!",
+                    Toast.LENGTH_SHORT).show();
+            new PostDataAsyncTask().execute();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Toast.makeText(MainActivity.this, provider + "'s status changed to " + status + "!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Toast.makeText(MainActivity.this, "Provider " + provider + " enabled!",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Toast.makeText(MainActivity.this, "Provider " + provider + " disabled!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class PostDataAsyncTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // do stuff before posting data
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String loc = ll.getText().toString();
+            String temp = "0,0";
+            try {
+                if (loc.length() == 0) {
+                    //Do nothing
+                    postText(temp);
+                } else {
+                    postText(loc);
+                }
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String lenghtOfFile) {
+            // do stuff after posting data
+        }
+    }
+
+    private void postText(String loc) {
+        try {
+            // url where the data will be posted
+
+            String postReceiverUrl = "http://www.embeddedcollege.org/rewebservices/mylocation.php";
+            Log.v("Attempt url", "postURL: " + postReceiverUrl);
+
+            // HttpClient
+            HttpClient httpClient = new DefaultHttpClient();
+
+            // post header
+            HttpPost httpPost = new HttpPost(postReceiverUrl);
+
+            // add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("location", loc));
+
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // execute HTTP post request
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity resEntity = response.getEntity();
+
+            if (resEntity != null) {
+
+                String responseStr = EntityUtils.toString(resEntity).trim();
+                Log.v("Status", "Response: " + responseStr);
+
+                // you can add an if statement here and do other actions based on the response
+            }
+
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastPress > 5000) {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_LONG).show();
+            lastPress = currentTime;
+        } else {
+            super.onBackPressed();
+
+        }
+    }
+}
